5c969b0820529aacbae1ecb24597e36421f0c084
Rohan Jha <rohan-jha@outlook.com>
2026-01-31 22:44:03 -0800

    Modified DataStructure-java

--- src/com/datastructures/Main.java
+++ src/com/datastructures/Main.java
@@ -1,8 +1,9 @@
+package src.com.datastructures;
 public class Main {
     public static void main(String[] args) {
         Stack stack = new Stack();
         for (int i = 0; i < 5; i++) {
-            stack.push(i);
+            stack.push("apple");
         }
         System.out.println(stack.getStack());
         stack.pop();
--- src/com/datastructures/Stack.java
+++ src/com/datastructures/Stack.java
@@ -1,27 +1,30 @@
+package src.com.datastructures;
+import java.util.List;
 import java.util.ArrayList;
 
-public class Stack {
-    public static void main(String[] args) {
 
+public class Stack<T> {
+    public static void main(String[] args) {
+        
     }
-    private ArrayList<Integer> items;
+    private List<T> items = new ArrayList<>();
 
     public Stack() {
         this.items = new ArrayList<>();
     }
-    public void push(Integer item) {
+    public void push(T item) {
         this.items.add(item);
     }
     public void pop() {
         this.items.remove(this.items.indexOf(this.items.getLast()));
     }
-    public int peek() {
+    public T peek() {
         return this.items.getLast();
     }
     public boolean isEmpty() {
         return this.items.size() == 0;
     }
-    public ArrayList<Integer> getStack() {
+    public List<T> getStack() {
         return this.items;
     }
 
